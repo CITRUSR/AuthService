6ee9e0aae7e9932cf8a446b80e33524ee2c57f3b
Citrus <savelijpetrov38@gmail.com>
2023-09-16 19:45:57 +0300

    Add JwtGeneration

---
+++ AuthService.Application/Jwt/AuthOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Permissions;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthService.Application.Jwt;
+
+public class AuthOptions
+{
+    public string Audience { get; set; }
+    public string Issuer { get; set; }
+    public string Secret { get; set; }
+    public int ExpiresTime { get; set; }
+    private readonly IConfiguration _configuration;
+
+    public AuthOptions(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        Audience = _configuration["Auth:Audience"];
+        Issuer = _configuration["Auth:Issuer"];
+        Secret = _configuration["Auth:Secret"];
+        ExpiresTime = Convert.ToInt32(_configuration["Auth:ExpressTimeInDays"]);
+    }
+}
---
+++ AuthService.Application/Jwt/JwtGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthService.Application.Jwt;
+
+public class JwtGenerator
+{
+    private readonly AuthOptions _authOptions;
+
+    public JwtGenerator(AuthOptions authOptions)
+    {
+        _authOptions = authOptions;
+    }
+
+    public string GenerateJWT(List<Claim> claims)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authOptions.Secret));
+        var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var jwt = new JwtSecurityToken(
+            audience: _authOptions.Audience,
+            issuer: _authOptions.Issuer,
+            expires: DateTime.UtcNow.AddDays(_authOptions.ExpiresTime),
+            claims: claims,
+            signingCredentials: signingCredentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(jwt);
+    }
+}
